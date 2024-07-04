ccc56271d5828e648a21c92be9a2ca59e859f581
CHia <dozeuto@gmail.com>
2024-07-04 15:03:46 +0800

    initial project

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./globals.css";
+import { inter } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/lib/utils.ts
@@ -0,0 +1,19 @@
+import { Revenue } from "./definitions";
+
+export const formatCurrency = (amount: number) => {
+  return (amount / 100).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+};
+
+export const formatDataToLocal = (dateStr: string, local: string = "en-US") => {
+  const date = new Date(dateStr);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+  const formatter = new Intl.DateTimeFormat(local, options);
+  return formatter.format(date);
+};
