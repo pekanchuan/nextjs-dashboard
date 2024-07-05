2d9e68a1189ce63cbdfa7da70bcbcaedefe6d6a7
CHia <dozeuto@gmail.com>
2024-07-05 15:41:29 +0800

    adding search and pagination

---
+++ app/ui/invoices/buttons.tsx
@@ -0,0 +1,11 @@
+export function CreateInvoice() {
+  return <></>;
+}
+
+export function UpdateInvoice({ id }: { id: string }) {
+  return <></>;
+}
+
+export function DeleteInvoice({ id }: { id: string }) {
+  return <></>;
+}
